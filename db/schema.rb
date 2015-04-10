934268ff3ba5355a058ce6d6f8d73d75743690b9
ayoub <ayoubben>
2015-04-10 22:45:26 +0200

    last

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150409194001) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boats", force: true do |t|
     t.string   "title"
     t.text     "description"
